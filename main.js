f615b3a8cbacc48ae9e022af4c081fcb07a360e9
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-06-15 16:39:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+predicition_1 = "";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function capture_image() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured_img' src='" + data_uri + "'>";
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/_ArEi9jD0/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model loaded");
+}
